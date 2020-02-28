fc9d1fbb36ef668a6ab69e08b9ce7ce8eed1b090
Jake Hao <hao+fork@gmail.com>
2020-02-29 07:07:37 +0800

    add window

--- FormKitExample/AppDelegate.swift
+++ FormKitExample/AppDelegate.swift
@@ -11,11 +11,21 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupKeyWindow()
         return true
+    }
+
+    private func setupKeyWindow() {
+        let vc = ViewController(nibName: nil, bundle: nil)
+        let navVC = UINavigationController(rootViewController: vc)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
     }
 
     // MARK: UISceneSession Lifecycle
--- FormKitExample/ViewController.swift
+++ FormKitExample/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        view.backgroundColor = .green
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
