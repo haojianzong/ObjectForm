6b7e8981e2b89e66c1c6fb2839b741cfe45da006
Jake Hao <hao+fork@gmail.com>
2020-02-29 07:13:58 +0800

    add form

--- FormKitExample/ViewController.swift
+++ FormKitExample/ViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .insetGrouped)
+        view.rowHeight = UITableView.automaticDimension
+        view.backgroundColor = .tertiarySystemBackground
+        return view
+    }()
+
     override func viewDidLoad() {
-        view.backgroundColor = .green
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+}
